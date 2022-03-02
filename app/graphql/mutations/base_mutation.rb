16a609d8701ebf6065069d72abf04a1ba02d6aa4
Morenikeji Bello <caeserselassie@gmail.com>
2022-03-02 22:10:09 +0100

    Add secret_key as enviroment variable

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -7,13 +7,11 @@ module Mutations
     input_object_class Types::BaseInputObject
     object_class Types::BaseObject
 
-    API_KEY = 'sk_test_7f6142a18b559e4324ce096554d0d81c629fe9a8'
-
     def verify_account(account_number, bank_code)     
       conn = Faraday.new(
         # Account Verification
         url: 'https://api.paystack.co',
-        headers: {'authorization' => "Bearer #{API_KEY}"}
+        headers: {'authorization' => "Bearer #{ENV['PAYSTACK_SECRET']}"}
       )
 
       resp = conn.get("bank/resolve?account_number=#{account_number}&bank_code=#{bank_code}")
