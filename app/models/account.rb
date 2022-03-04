73824a5b7810006706844d3fa93041cff3bd5c1e
Morenikeji Bello <caeserselassie@gmail.com>
2022-03-04 12:09:04 +0100

    Write validation tests for Account Model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,7 @@
 class Account < ApplicationRecord
-    validates :user_account_name, presence: true, length: { minimum: 2 }
+    validates :user_account_name, presence: true
     validates :user_account_number, presence: true, length: { maximum: 10, minimum: 10 }
-    validates :bank_code, presence: true
+    validates :bank_code, presence: true, length: { maximum: 3, minimum: 3 }
 
     belongs_to :user
 end
